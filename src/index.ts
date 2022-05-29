efccfb4abd24aadfd02f75589fd1f931559f8a24
pandapedro <pandapedro001@gmail.com>
2022-05-29 17:41:35 -0300

    ok

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
+import "./utilities/i18n";
 import { Client } from 'eris';
+import "./utilities/ModuleAlias";
 import DefaultCommand from './interfaces/Command';
 
 export default class Bot extends Client {
